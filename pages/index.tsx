60d384fd732fe09ad3fbd8f6ebdd1052b66ef771
Lee <43488305+poiu694@users.noreply.github.com>
2023-01-31 23:48:27 +0900

    chore: recoil boilerplate 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useRecoilState } from 'recoil';
+
+import styles from '../styles/Home.module.css';
+import { sampleCountState } from '../src/store';
 
 const Home: NextPage = () => {
+  const [count, setCount] = useRecoilState<number>(sampleCountState);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,8 +23,10 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Count : {count}
+          <button type="button" onClick={() => setCount((prev) => prev + 1)}>
+            Click
+          </button>
         </p>
 
         <div className={styles.grid}>
@@ -33,10 +40,7 @@ const Home: NextPage = () => {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -46,9 +50,7 @@ const Home: NextPage = () => {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
@@ -66,7 +68,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
