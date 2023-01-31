a32713121b7925a9ef6d6051087affa77793f274
Lee <43488305+poiu694@users.noreply.github.com>
2023-02-01 00:20:03 +0900

    feat: recoil devtool 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,29 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
+import React from 'react';
+import { RecoilRoot, useRecoilSnapshot } from 'recoil';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { QueryClient, QueryClientProvider } from 'react-query';
+
+function RecoilDebugObserver() {
+  const snapshot = useRecoilSnapshot();
+  
+  React.useEffect(() => {
+    console.debug('The following atoms were modified:');
+    for (const node of snapshot.getNodes_UNSTABLE({ isModified: true })) {
+      console.debug(node.key, snapshot.getLoadable(node));
+    }
+  }, [snapshot]);
+
+  return null;
+}
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      {process.env.NODE_ENV === 'development' && <RecoilDebugObserver />}
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
         <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
