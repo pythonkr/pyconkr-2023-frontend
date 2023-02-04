a879b345a6e4943716eb6d55239d603cce40039b
alreadynyeong <kjw87130@naver.com>
2023-02-05 00:39:08 +0900

    Add: Toggle Component

---
+++ components/Toggle.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { styled } from '../stitches.config';
+import { H4 } from './heading';
+import ArrowDownSimple from 'public/ArrowDownSimple.svg';
+import ArrowUpSimple from 'public/ArrowUpSimple.svg';
+
+const Button = styled('button', {
+  background: 'none',
+  border: 'none',
+});
+
+const Show = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  marginTop: 16,
+  marginBottom: 20,
+  color: '$textPrimary',
+});
+
+const Hide = styled('p', {
+  bodyText: 1,
+  color: '$textPrimary',
+});
+
+interface ToggleProps {
+  title?: string;
+  content?: string;
+}
+
+const Toggle = (props: ToggleProps) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const { title, content } = props;
+
+  return (
+    <>
+      <hr />
+      <Show>
+        <H4>{title}</H4>
+        <Button onClick={() => setOpen((isOpen) => !isOpen)}>
+          {open ? <ArrowUpSimple /> : <ArrowDownSimple />}
+        </Button>
+      </Show>
+      {open ? <Hide>{content}</Hide> : null}
+    </>
+  );
+};
+
+export default Toggle;
--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
     loader: "imgix",
     path: "/",
   },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
+  }
 }
 
 module.exports = nextConfig
