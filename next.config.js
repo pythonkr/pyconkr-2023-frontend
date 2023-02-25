7d3c9f107540eb5bcba8ee98f905d01ad6d28bf9
gollumnima <doori.alice.kim@gmail.com>
2023-02-26 01:09:53 +0900

    이미지 외부 경로 설정 변경 (추후 수정해야)

--- next.config.js
+++ next.config.js
@@ -3,8 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    loader: 'imgix',
-    path: '/',
+    // TODO: domain 추후 수정하기
+    domains: ['cdn.pixabay.com', '**'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**',
+      },
+    ],
   },
   webpack(config) {
     config.module.rules.push({
