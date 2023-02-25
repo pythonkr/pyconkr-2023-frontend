3493a31caf98ea9cc8cb6f755edc86e5c73d6245
constmoon <jiwoo.link@gmail.com>
2023-02-26 03:41:49 +0900

    coc 페이지 생성

---
+++ constants/cocIndex.ts
@@ -0,0 +1,38 @@
+import { RouteType } from '@/interfaces/RouteType';
+
+const cocIndex: RouteType[] = [
+  {
+    route: '#1',
+    title: '의도와 목적',
+  },
+  {
+    route: '#2',
+    title: '커뮤니티',
+  },
+  {
+    route: '#3',
+    title: '의사소통',
+  },
+  {
+    route: '#4',
+    title: '프라이버시',
+  },
+  {
+    route: '#5',
+    title: '영리 행위',
+  },
+  {
+    route: '#6',
+    title: '공중 도덕',
+  },
+  {
+    route: '#7',
+    title: '신고하기',
+  },
+  {
+    route: '#8',
+    title: '기여하기',
+  },
+];
+
+export default cocIndex;
