adff2d9fab02e778ad6c004d44e7c25b2d0a4075
constmoon <jiwoo.link@gmail.com>
2023-02-26 16:20:09 +0900

    /coc 접근시 리다이렉트 추가

--- pages/coc/index.tsx
+++ pages/coc/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import { useRouter } from 'next/router';
 import type { NextPage } from 'next';
 
 // TODO: CoC head og 추가
 const CoCPage: NextPage = () => {
+  const router = useRouter();
+  router.replace('/coc/purpose');
+
   return <></>;
 };
 
