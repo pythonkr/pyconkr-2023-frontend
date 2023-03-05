b24d851fd179f271a30b3ff187b9c76345a1b47f
constmoon <jiwoo.link@gmail.com>
2023-03-06 03:23:55 +0900

    og 이미지 루트 변경

--- constants/metaTags.ts
+++ constants/metaTags.ts
@@ -2,7 +2,7 @@ export const DefaultMetaTags = {
   title: '파이콘 한국 2023',
   description: '파이콘 한국 2023 홈페이지',
   imageUrl:
-    'https://pyconkr-web-2023-static.s3.ap-northeast-2.amazonaws.com/og_720.jpg',
+    'https://pyconkr-web-2023-static.s3.ap-northeast-2.amazonaws.com/pyconkr-2023-og.png',
   siteUrl:
     process.env.NODE_ENV === 'production'
       ? 'https://2023.pycon.kr'
