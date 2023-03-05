8460a56ce5d42314e5c8e901e241af20b5236a4f
constmoon <jiwoo.link@gmail.com>
2023-03-06 03:46:46 +0900

    coc og description 추가

--- pages/coc/index.tsx
+++ pages/coc/index.tsx
@@ -1,13 +1,21 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import SeoHeader from '@/components/layout/SeoHeader';
+import { Routes } from '@/constants/routes';
 
-// TODO: CoC head og 추가
 const CoCPage = () => {
   const router = useRouter();
 
   useEffect(() => {
     router.replace('/coc/purpose');
   }, [router]);
+
+  return (
+    <SeoHeader
+      title={Routes.COC.title}
+      description="파이콘 한국 2023: 8월 11~13일 코엑스"
+    />
+  );
 };
 
 export default CoCPage;
