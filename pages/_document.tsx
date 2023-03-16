76a066b15f85439933cbbe59f56354a0a602eec6
blurfx <iam@xo.dev>
2023-03-17 02:11:57 +0900

    fix: page background header

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import { getCssText } from '@/stitches.config';
+import { getCssText, globalCss } from '@/stitches.config';
 
+const globalStyle = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+    fontFamily:
+      '"Spoqa Han Sans Neo", -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif',
+
+    '&:lang(ko)': {
+      wordBreak: 'keep-all',
+    },
+  },
+
+  'html, body, #__next': {
+    height: '100%',
+    minHeight: '100%',
+    background: '$backgroundPrimary',
+  },
+
+  a: {
+    color: 'inherit',
+    textDecoration: 'none',
+  },
+});
 export default class Document extends NextDocument {
   render() {
+    globalStyle();
     return (
       <Html lang="ko">
         <Head>
