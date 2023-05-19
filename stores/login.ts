c85168c143c5ac3f7853a6562ab5743f3557eaed
Hyeok Min Kwon <khmzxc@naver.com>
2023-05-20 00:22:12 +0900

    Add comment

--- stores/login.ts
+++ stores/login.ts
@@ -4,6 +4,6 @@ import { atom } from 'recoil';
 export const userState = atom<LoginUser>({
   key: 'user',
   default: {
-    userid: null,
+    userid: null, // TODO recoil-persist 같은 걸로 새로 고침해도 유지되게
   },
 });
