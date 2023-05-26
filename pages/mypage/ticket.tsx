52bdc643333415b42f6b11933e9cf9f903b28a92
NaGyeong Park <ngp.july@gmail.com>
2023-05-26 21:52:13 +0900

    feat: mypage ticket 페이지 개발 : API 미연결

---
+++ pages/mypage/ticket.tsx
@@ -0,0 +1,155 @@
+import { H1 } from '@/components/heading';
+import SubNavBar from '@/components/layout/SubNavBar';
+import type { NextPage } from 'next';
+import { styled } from 'stitches.config';
+import mypageIndex from '@/constants/mypageIndex';
+import { useCallback, useEffect, useState } from 'react';
+import { Loader } from '@/components/common/Loader';
+import { useRouter } from 'next/router';
+import { Routes } from '@/constants/routes';
+import Button from '@/components/common/Button';
+import { MyTicketType } from '@/@types/mypage';
+import Link from 'next/link';
+import { StyledH4 } from '@/components/common/Markdown';
+import { BodyText } from '@/components/sponsor/information/styles';
+
+const Layout = styled('div', {
+  width: '100%',
+  height: '100%',
+  maxWidth: '1280px',
+  margin: '0 auto',
+  paddingLeft: '2rem',
+  paddingRight: '2rem',
+});
+
+const StyledH1 = styled(H1, {
+  marginTop: '70px',
+});
+
+const SubText = styled('div', {
+  fontSize: '32px',
+  marginTop: '16px',
+  color: '$textSecondary',
+  fontWeight: 'bold',
+});
+
+const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  marginTop: 40,
+  paddingBottom: 40,
+  '@bp2': {
+    marginTop: '90px',
+    flexDirection: 'row',
+  },
+});
+
+const TicketBox = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  margin: '10% 0',
+  borderBottom: '4px solid $textPrimary',
+  '@bp2': {
+    margin: '0 5%',
+  },
+});
+
+const TicketItem = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  borderTop: '4px solid $textPrimary',
+  padding: '14px 10px',
+});
+
+const TicketInfo = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  '& > h4': {
+    padding: '0px',
+  },
+});
+
+const MyTicketPage: NextPage = () => {
+  const [myTicket, setMyTicket] = useState<MyTicketType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const loadMyTicket = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      setMyTicket(
+        MyTicketType.fromAPIs([
+          {
+            id: 'ticket1',
+            name: '일반 티켓 (8/13)',
+            price: 50000,
+            payment_date: '2021-07-01T00:00:00+09:00',
+            is_refundable: true,
+          },
+          {
+            id: 'ticket2',
+            name: '튜토리얼 (8/13)',
+            price: 30000,
+            payment_date: '2021-07-01T00:00:00+09:00',
+            is_refundable: true,
+          },
+          {
+            id: 'ticket3',
+            name: '스프린트 (8/13)',
+            price: 20000,
+            payment_date: '2021-07-01T00:00:00+09:00',
+            is_refundable: true,
+          },
+        ])
+      );
+    } catch (e) {
+      alert(`나의 티켓 목록 불러오기 실패\n(${e})`);
+      router.push(Routes.HOME.route);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [router]);
+
+  useEffect(() => {
+    loadMyTicket();
+  }, [loadMyTicket]);
+
+  if (isLoading === true)
+    return (
+      <>
+        <Loader />
+      </>
+    );
+  return (
+    <Layout>
+      <StyledH1>마이페이지</StyledH1>
+      <SubText>Mypage</SubText>
+      <Container>
+        <SubNavBar routes={mypageIndex} />
+        <TicketBox>
+          {myTicket.map((ticket) => (
+            <TicketItem key={ticket.name}>
+              <TicketInfo>
+                <StyledH4>{ticket.name}</StyledH4>
+                <BodyText>{`${ticket.price.toLocaleString()}원`}</BodyText>
+              </TicketInfo>
+              <Link
+                href={Routes.MYPAGE_REFUND.route + `/${ticket.id}`}
+                passHref
+              >
+                <Button size="small" reversal>
+                  환불하기
+                </Button>
+              </Link>
+            </TicketItem>
+          ))}
+        </TicketBox>
+      </Container>
+    </Layout>
+  );
+};
+
+export default MyTicketPage;
