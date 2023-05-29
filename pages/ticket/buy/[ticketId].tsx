bba4c4786b1ee235456626f395b21431d7219270
Hyeok Min Kwon <khmzxc@naver.com>
2023-05-29 13:00:26 +0900

    Add showing module in ticket page

--- pages/ticket/buy/[ticketId].tsx
+++ pages/ticket/buy/[ticketId].tsx
@@ -3,10 +3,13 @@ import { Routes } from '@/constants/routes';
 import { userState } from '@/stores/login';
 import { ticketState } from '@/stores/ticket';
 import { useRouter } from 'next/router';
-import React, { useEffect, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import { useRecoilValue } from 'recoil';
 import * as S from '@/components/ticket/styles';
 import Button from '@/components/common/Button';
+import IMP from 'lib/portone';
+import { PaymentAPI } from '@/api';
+import { announcePaymentSucceeded } from '@/api/payment';
 
 const TicketBuyPage = () => {
   const router = useRouter();
@@ -28,6 +31,62 @@ const TicketBuyPage = () => {
       router.push(Routes.HOME.route);
   }, [loginUser, router, selectedTicketType]);
 
+  /**
+   * 1. 결제하기를 누르면 payment_key 생성하는 API 호출
+   * 2. payment_key 생성되면 PG 모듈 띄우기
+   * 3. 결제 완료되면 결제 성공 여부를 서버에 보내줌
+   */
+
+  /** PG 모듈 띄우기 */
+  const requestPay = useCallback(
+    (paymentKey: string) => {
+      if (selectedTicketType === undefined) return;
+
+      IMP.request_pay(
+        {
+          pg: 'html5_inicis.INIpayTest',
+          pay_method: 'card',
+          merchant_uid: paymentKey,
+          name: selectedTicketType.name,
+          amount: selectedTicketType.price,
+          buyer_name: '권혁민', // TODO
+          buyer_email: '',
+          buyer_tel: '01000000000',
+          buyer_addr: '',
+          buyer_postcode: '',
+        },
+        async (response) => {
+          console.log(response);
+          // 결제 성공 여부 전송
+          if (response.success === true)
+            await announcePaymentSucceeded(paymentKey);
+        }
+      );
+    },
+    [selectedTicketType]
+  );
+
+  const buyTicket = useCallback(async () => {
+    if (selectedTicketType === undefined) return;
+
+    /////////////////////////////////////////////////////
+    // payment_key 생성
+    /////////////////////////////////////////////////////
+    let paymentKey: Awaited<ReturnType<(typeof PaymentAPI)['getPaymentKey']>>;
+    try {
+      paymentKey = await PaymentAPI.getPaymentKey(selectedTicketType);
+    } catch (e) {
+      alert(`결제 준비 실패 (${e})`);
+      router.push(Routes.TICKET.route);
+      return;
+    }
+    /////////////////////////////////////////////////////
+    // PG 모듈 띄우기
+    /////////////////////////////////////////////////////
+    requestPay(paymentKey);
+    /////////////////////////////////////////////////////
+  }, [router, requestPay, selectedTicketType]);
+
   return (
     <S.TicketOrderContainer>
       <S.TicketOrderTitle>Order</S.TicketOrderTitle>
@@ -36,7 +95,7 @@ const TicketBuyPage = () => {
           {selectedTicketType?.name}
         </S.TicketTypeDetailTitle>
         <S.TicketTypeDetailProgramType>
-          {selectedTicketType?.program} TICKET
+          {selectedTicketType?.program.programType} TICKET
         </S.TicketTypeDetailProgramType>
         <S.TicketTypeDetailDesc>
           {selectedTicketType?.desc}
@@ -51,7 +110,7 @@ const TicketBuyPage = () => {
         <div>{selectedTicketType?.price.toLocaleString()}원</div>
       </S.TicketOrderPrice>
       <S.TicketOrderButton>
-        <Button size="big" reversal>
+        <Button size="big" reversal onClick={buyTicket}>
           결제하기
         </Button>
       </S.TicketOrderButton>
