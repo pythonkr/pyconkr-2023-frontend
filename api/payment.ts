e003a29540e431c06a76fbd17d0b4378bc6b4124
Hyeok Min Kwon <khmzxc@naver.com>
2023-05-29 13:36:10 +0900

    Add refund API

--- api/payment.ts
+++ api/payment.ts
@@ -2,6 +2,10 @@ import { TicketType } from '@/@types';
 import axios from '@/lib/axios';
 import { AxiosResponse } from 'axios';
 
+/**
+ * 결제를 위한 payment_key 생성
+ * payment_key는 merchant_uid로 쓰인다.
+ */
 export function getPaymentKey(ticketType: TicketType): Promise<string> {
   return new Promise((resolve, reject) => {
     axios
@@ -31,6 +35,10 @@ export function getPaymentKey(ticketType: TicketType): Promise<string> {
   });
 }
 
+/**
+ * 결제 모듈의 결제 결과를 서버에 알려줌
+ * @param marchantUid payment_key
+ */
 export function announcePaymentSucceeded(marchantUid: string): Promise<void> {
   return new Promise((resolve, reject) => {
     axios
@@ -42,6 +50,39 @@ export function announcePaymentSucceeded(marchantUid: string): Promise<void> {
         `/payment/success`,
         {
           merchant_uid: marchantUid,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        resolve();
+      })
+      .catch((e) => {
+        console.error(e);
+        reject(e);
+      });
+  });
+}
+
+/**
+ * 결제 취소 (환불)
+ * @param paymentKey payment_key
+ */
+export function cancelPayment(paymentKey: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    axios
+      .post<
+        { msg: string },
+        AxiosResponse<{ msg: string }>,
+        { payment_key: string }
+      >(
+        `/payment/refund`, // TODO
+        {
+          payment_key: paymentKey,
         },
         {
           headers: {
