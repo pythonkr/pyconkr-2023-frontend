fa1454e5d428bf982020d8b126869804ae03400d
alreadynyeong <kjw87130@naver.com>
2023-07-03 13:08:39 +0900

    api url 변경

--- api/session.ts
+++ api/session.ts
@@ -2,6 +2,6 @@ import axios from '@/lib/axios';
 import { SessionList } from '@/@types/session';
 
 export async function getSessionList(): Promise<SessionList[]> {
-  const response = await axios.get('https://api-dev.pycon.kr/sessions/');
+  const response = await axios.get(`/sessions/`);
   return response.data;
 }
