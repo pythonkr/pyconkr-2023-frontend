6f3358fe9bfadc496456649680290cb5d9057c5f
NaGyeong Park <ngp.july@gmail.com>
2023-08-02 23:31:55 +0900

    fix: 바뀐 sessionDetail api type 수정 및 api call 수정

--- @types/session.ts
+++ @types/session.ts
@@ -16,21 +16,20 @@ export interface SessionList {
 export interface SessionDetail {
   id: number;
   title: string;
-  brief?: string; // 리뷰용: 발표에 대한 간단한 설명.
-  desc?: string; // 리뷰용: 발표에 대한 자세한 설명
-  comment?: string; // 리뷰용: 파준위에게 전하고 싶은 말
   difficulty: SessionDifficulty;
   duration: SessionDuration;
   language: SessionLanguage;
   category: number;
   category_name: string;
-  accepted: boolean;
   introduction: string;
   video_url: string | null;
   slide_url: string | null;
   room_num: SessionRoomNumber;
-  created_at: string;
-  updated_at: string;
+  day_of_week: 'Sat' | 'Sun' | null;
+  start_at: string;
+  host_name: string;
+  host_introduction: string;
+  host_profile_image: string | null;
   user: User | null;
 }
 
