057f937a463431cbf3ecb0a954b373a78766a1c3
Hyeok Min Kwon <khmzxc@naver.com>
2023-08-09 07:37:53 +0900

    Add organizing team const data

--- constants/about.ts
+++ constants/about.ts
@@ -7,15 +7,89 @@ export const CommitteePageInfo = {
 };
 export const CommitteeList: CommitteeInfo[] = [
   {
-    name: '테스트',
-    image:
-      'https://mblogthumb-phinf.pstatic.net/MjAxOTA3MTVfMjA0/MDAxNTYzMTc2Mzc5NTAy.lh9RRCYZCuuD_nYPyNdbhiJzdd7_YuUxfyzTWHX1flEg.sL2fBPI0Iglgm1lILEORTWRyb66n6PXgBLf2c2eyuiYg.JPEG.petgeek/cici.toto.mametchi_55864983_674522579671640_592800947380049308_n.jpg?type=w800',
-    description:
-      '안녕하세요? 귀여운 고양이입니다. 파이콘 자원봉사자로 활동하고 있습니다. 파이콘 사랑해요!',
+    name: '강지우',
+    image: undefined,
+    description: 'https://constmoon.github.io',
   },
   {
-    name: '이미지 없는 테스트2',
+    name: '권혁민',
+    image: undefined,
+    description: 'Pie, Remidomine... Benedicite!',
+  },
+  { name: '김강민', image: undefined, description: '다시, 우리, 파이썬' },
+  {
+    name: '콘파이',
+    image: undefined,
+    description: '파이썬은 모르지만 콘파이 맛있어 yum ',
+  },
+  { name: '김서현', image: undefined, description: '다시, 우리, 파이썬' },
+  {
+    name: '김수빈',
+    image: undefined,
+    description: '좋아하는 일을 잘할 수 있도록!',
+  },
+  {
+    name: '김순태',
+    image: undefined,
+    description: '다양한 경험을 좋아하고 파이썬으로 개발하는 개발자입니다.',
+  },
+  {
+    name: '노하은',
+    image: undefined,
     description:
-      '안녕하세요? 테스트입니다.. 파이콘 자원봉사자로 활동하고 있습니다. 파이콘 사랑해요!',
+      '파이썬과 IT 기술을 사랑하고 경험과 지식을 나누는 행사에 기여하는 것을 행복으로 살고 있습니다. 이 마음을 담은 IT 교육 기획/운영을 하고 있습니다.',
+  },
+  {
+    name: 'Jess',
+    image: undefined,
+    description: 'DEV AnythinG / https://github.com/dev-bearabbit',
+  },
+  { name: '민예지', image: undefined, description: '다시, 우리, 파이썬' },
+  {
+    name: 'golony6449',
+    image: undefined,
+    description: '백엔드 개발을 하고 있습니다. 근데 이제 인프라를 곁들인',
+  },
+  { name: '배권한', image: undefined, description: '파이썬을 사랑합니다.' },
+  { name: '심명진', image: undefined, description: '다시, 우리, 파이썬' },
+  {
+    name: '오인경',
+    image: undefined,
+    description: '성장, 발전 그리고 사람에게 진심인 기획자',
+  },
+  {
+    name: '이영은',
+    image: undefined,
+    description:
+      '재미있는 모든 것들을 좋아합니다\nhttps://github.com/oleveloper',
+  },
+  {
+    name: 'WooSub Lee',
+    image: undefined,
+    description: 'LoveFrom / https://www.linkedin.com/in/woosublee',
+  },
+  { name: '이재준', image: undefined, description: '다시, 우리, 파이썬' },
+  { name: '이준원', image: undefined, description: '다시, 우리, 파이썬' },
+  {
+    name: 'Han Lee | 이한결 (한결같은한결, 한결같은 개발자)',
+    image: undefined,
+    description:
+      '작년에 키노트 연사로 처음 파이콘에 기여를 시작하여, 올해는 운이 좋게도 파이콘 한국 준비위원회에 합류해 기여하게 되었습니다.\n이전에 즐거운 추억을 제게 선사했던 파이콘 한국을 이번에는 직접 준비하고 개최, 운영하는데 기여할 수 있어 기쁩니다.\n올해 행사를 준비하면서 부족한 점이 많았지만 함께해주신 다른 준비 위원분들 덕분에 부족한 부분을 점점 채워, 성장하며 기여할 수 있었습니다.\n이번 행사도 물론이고 앞으로 파이썬 커뮤니티에 뜻깊게 기여할 수 있도록 노력해 나가겠습니다!',
+  },
+  { name: '이현호', image: undefined, description: '다시, 우리, 파이썬' },
+  {
+    name: '정동규 / yummygyudon',
+    image: undefined,
+    description: '🌟 동반 성장과 기여에 관심이 많은 학생입니다. 🌟',
+  },
+  {
+    name: '조용주 Peniel Cho',
+    image: undefined,
+    description: '요즘 관심사는 키움 히어로즈',
+  },
+  {
+    name: '진태양(Theo)',
+    image: undefined,
+    description: '주니어 CTO로 일하고 있습니다.',
   },
 ];
